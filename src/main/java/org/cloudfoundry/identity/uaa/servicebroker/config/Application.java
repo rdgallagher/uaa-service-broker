3b9378f336309266b92ea8aa2506f35a61fc1d1c
Robert Gallagher <pair+rgallagher@pivotallabs.com>
2014-05-20 11:39:53 -0700

    Add Spring Security OAuth - Enable OAuth client - Configure UAA resource

--- src/main/java/org/cloudfoundry/identity/uaa/servicebroker/config/Application.java
+++ src/main/java/org/cloudfoundry/identity/uaa/servicebroker/config/Application.java
@@ -1,15 +1,54 @@
 package org.cloudfoundry.identity.uaa.servicebroker.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.token.AccessTokenRequest;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
+import java.util.Arrays;
+import javax.annotation.Resource;
+
+@Configuration
 @EnableAutoConfiguration
+@EnableOAuth2Client
 @ComponentScan
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-    	
+
+    @Configuration
+    protected static class OAuth2Config {
+
+        @Resource
+        @Qualifier("accessTokenRequest")
+        private AccessTokenRequest accessTokenRequest;
+
+        @Bean
+        @Scope(value = "session", proxyMode = ScopedProxyMode.INTERFACES)
+        public OAuth2RestTemplate uaaRestTemplate() {
+            return new OAuth2RestTemplate(uaa(), new DefaultOAuth2ClientContext(accessTokenRequest));
+        }
+
+        @Bean
+        public ClientCredentialsResourceDetails uaa() {
+            ClientCredentialsResourceDetails uaa = new ClientCredentialsResourceDetails();
+            uaa.setId("uaa");
+            uaa.setClientId("admin");
+            uaa.setClientSecret("adminsecret");
+            uaa.setAccessTokenUri("http://localhost:8080/uaa/oauth/token");
+            uaa.setScope(Arrays.asList("clients.read", "clients.write"));
+            return uaa;
+        }
+    }
 }
